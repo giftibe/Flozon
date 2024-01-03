6bd8125d6a174cf809b01f6c371933993eade417
Gift <Giftibe62@gmail.com>
2024-01-03 16:18:31 +0100

    feat: added cart prior to checkout

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,4 +1,7 @@
 import { getCart } from "@/lib/db/carts"
+import CartEntry from "./CartEntry"
+import setProductQuantity from "./actions"
+import formatPrice from "@/lib/formatPrice"
 
 export const metadata = {
     title: 'Your cart - Flozon'
@@ -12,10 +15,21 @@ export default async function CartPage() {
         <div>
             <h1 className="mb-3 text-3xl font-bold">Shopping Cart</h1>
             {
-                cart?.items.map(cartItem)=>{
-
+                cart?.items.map(cartItem => (
+                    <CartEntry
+                        cartItem={cartItem}
+                        key={cartItem.id}
+                        setProductQuantity={setProductQuantity}
+                    />
+                ))
             }
-            }
+            {!cart?.items.length && <p>Your cart is empty</p>}
+            <div className="flex flex-col items-end sm:items-center" >
+                <p className="mb-3 font-bold">
+                    Total: {formatPrice(cart?.subtotal || 0)}
+                </p>
+                <button className="btn btn-primary sm:w-[200px]">Checkout</button>
+            </div>
         </div>
     )
 }

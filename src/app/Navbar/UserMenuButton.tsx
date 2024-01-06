b2fa7f19843984949fcf718688e5c4ad82ee3ecf
Gift <Giftibe62@gmail.com>
2024-01-06 17:51:13 +0100

    feat: user authorization

--- src/app/Navbar/UserMenuButton.tsx
+++ src/app/Navbar/UserMenuButton.tsx
@@ -3,6 +3,7 @@
 import { Session } from "next-auth"
 import Image from "next/image"
 import profilePicPlaceholder from "@/assets/profile-pic-placeholder.png"
+import { signIn, signOut } from "next-auth/react"
 
 interface UserMenuButtonProps {
     session: Session | null
@@ -39,6 +40,21 @@ export default function UserMenuButton({ session }: UserMenuButtonProps) {
                 )}
             </label>
 
-        </div>
+            <ul
+                tabIndex={0}
+                className="dropdown-content menu rounded-box menu-sm z-30 mt-3 w-52 bg-base-100 p-2 shadow"
+            >
+                <li>
+                    {
+                        user ?
+                            <button onClick={() => signOut({ callbackUrl: '/' })}>Sign Out</button>
+                            :
+                            <button onClick={() => signIn()}>Sign In</button>
+                    }
+                </li>
+
+            </ul>
+
+        </div >
     )
 }

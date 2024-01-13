cd5609798db7970471ec8ab454c4ed812a0c24d3
Gift <Giftibe62@gmail.com>
2024-01-13 01:05:59 +0100

    chore: restructured authorization options

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -2,7 +2,9 @@ import { prisma } from "@/lib/db/prisma"
 import FormSubmitButtom from '@/components/FormSubmitButtom'
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+// import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../../lib/authOptions";
+
 export const metadata = {
     title: 'Add Product - Flozon'
 }
